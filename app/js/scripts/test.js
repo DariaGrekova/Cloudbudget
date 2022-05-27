7ba6bd227e33bd609d1481c73a40ab463eab57ca
dga <gda@n-e.ru>
2022-05-27 11:09:37 +0300

    added style for kebab-menu

--- app/js/scripts/test.js
+++ app/js/scripts/test.js
@@ -1,3 +1,13 @@
 export function burgerMenu() {
+  const menuBtn = document.querySelector('.kebab-menu');
+  const menu = document.querySelector('.header-menu');
 
+  menuBtn.addEventListener('click', function () {
+    const menuItems = document.querySelectorAll('.header-menu__item');
+    menuItems.forEach((item) => {})
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    //bg.classList.toggle('active');
+    //document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
+  })
 }
