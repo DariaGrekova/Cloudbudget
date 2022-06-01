b62c2a91b55633a74ae9c9e283ee85c12a87d804
Daria Grekova <dg94@bk.ru>
2022-06-01 12:10:05 +0300

    fixed styles for mobile-menu

--- app/js/scripts/test.js
+++ app/js/scripts/test.js
@@ -1,10 +1,8 @@
 export function burgerMenu() {
-  const menuBtn = document.querySelector('.kebab-menu');
-  const menu = document.querySelector('.header-menu');
+  const menuBtn = document.querySelector('.arrow');
+  const menu = document.querySelector('.header__block');
 
   menuBtn.addEventListener('click', function () {
-    const menuItems = document.querySelectorAll('.header-menu__item');
-    menuItems.forEach((item) => {})
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
     //bg.classList.toggle('active');
